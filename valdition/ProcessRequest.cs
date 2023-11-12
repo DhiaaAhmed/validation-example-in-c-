53dda8fdc790d2208fff6594d6f4e2708cb4c1b9
DhiaaAhmed <123327759+DhiaaAhmed@users.noreply.github.com>
2023-11-12 22:43:33 +0300

    Add files via upload

---
+++ valdition/ProcessRequest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace valdition
+{
+    public class ProcessRequest
+    {
+        public String Name { get; set; }
+        public String Major { get; set; }
+        public int Year { get; set; }
+        public String Description { get; set; }
+        public int BondId { get; set; }
+        public ProcessRequest(String name, String major,int year, String description,int bondId)
+        {
+            Name = name;
+            Major = major;
+            Year = year;
+            Description = description;
+            BondId = bondId;
+
+        }
+
+    }
+
+    public class ProcessRequestValditor : AbstractValidator<ProcessRequest>
+    {
+        public ProcessRequestValditor()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+            RuleFor(x => x.Major).NotEmpty().WithMessage("Major is required");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required");
+            RuleFor(x => x.Year).InclusiveBetween(1,4).WithMessage("year of study must be between 1 and 4");
+            RuleFor(x => x.BondId).InclusiveBetween(1122, 1122).WithMessage("there is no bond with this id");
+        }
+    }
+}
---
+++ valdition/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using valdition;
+
+namespace Valdition
+{
+    class program
+    {
+        static void Main (String[] args)
+        {
+            var processRequest = new ProcessRequest("dhiaa","It",5,"وقف قيد",1122);
+            var valditor = new ProcessRequestValditor();
+            var result = valditor.Validate(processRequest);
+            if (!result.IsValid)
+            {
+                foreach (var error in result.Errors)
+                {
+                    Console.WriteLine(error.ErrorMessage);
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Done successfully\n");
+            }
+            Console.WriteLine("finished");
+            Console.ReadLine();
+        }
+    }
+}
